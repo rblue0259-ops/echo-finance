bbd5f390cb29e1f8a5ccd94f8d3f9dbab0c29a61
rblue0259-ops <rblue0259@gmail.com>
2026-07-13 23:02:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-console.log("Echo Finance Loaded Successfully!");
+let income=0;
+let expense=0;
+let investment=0;
 
-alert("Welcome to Echo Finance 🚀");
+function update(){
+document.getElementById("income").innerHTML="₹"+income;
+document.getElementById("expense").innerHTML="₹"+expense;
+document.getElementById("investment").innerHTML="₹"+investment;
+document.getElementById("balance").innerHTML="₹"+(income-expense-investment);
+}
+
+function addIncome(){
+let amount=prompt("Enter Income");
+if(amount){
+income+=Number(amount);
+update();
+}
+}
+
+function addExpense(){
+let amount=prompt("Enter Expense");
+if(amount){
+expense+=Number(amount);
+update();
+}
+}
+
+function addInvestment(){
+let amount=prompt("Enter Investment");
+if(amount){
+investment+=Number(amount);
+update();
+}
+}
+
+update();
